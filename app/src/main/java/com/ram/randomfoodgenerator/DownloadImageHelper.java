72ac0473f8faa734d3aa132b1d77f83c00e03e22
abbottli <abbott.li49@gmail.com>
2016-09-19 21:55:38 -0400

    ignoring important stuff

--- app/src/main/java/com/ram/randomfoodgenerator/DownloadImageHelper.java
+++ app/src/main/java/com/ram/randomfoodgenerator/DownloadImageHelper.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ImageView;
 
+import java.io.InputStream;
 import java.net.URL;
 
 /**
@@ -18,15 +19,15 @@ public class DownloadImageHelper extends AsyncTask<String, Void, Bitmap> {
         this.im = im;
     }
 
-
     @Override
     protected Bitmap doInBackground(String... params) {
         String imgURL = params[0];
 
         Bitmap bm = null;
         try {
-            URL url = new URL(imgURL);
-            Bitmap bmp = new BitmapFactory().decodeStream(url.openConnection().getInputStream());
+            Log.i(MainActivity.TAG, "Getting img");
+            InputStream in = new java.net.URL(imgURL).openStream();
+            bm = BitmapFactory.decodeStream(in);
         } catch (Exception e) {
             Log.e(MainActivity.TAG, "Image download encountered error: " + e.getMessage());
         }
